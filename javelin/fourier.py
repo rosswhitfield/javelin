aa7a6f3b3c952c0339507a5c3c9160c673dcc173
Ross Whitfield <whitfieldre@ornl.gov>
2016-04-12 17:00:39 -0400

    Add property getter/setter to Fourier

--- javelin/fourier.py
+++ javelin/fourier.py
@@ -4,31 +4,87 @@ import periodictable
 
 
 class Fourier(object):
-    __radiation_type = ["neutrons", "xray", "electrons"]
 
     def __init__(self):
-        self.structure = None
-        self.radiation = 'neutrons'
-        self.wavelenght = 1.54
-        self.lots = None
-        self.average = 0.0
-        self.na = 101
-        self.no = 101
-        self.ll = np.array([0.0, 0.0, 0.0])
-        self.lr = np.array([2.0, 0.0, 0.0])
-        self.ul = np.array([0.0, 2.0, 0.0])
+        self._structure = None
+        self._radiation = 'neutrons'
+        self._wavelenght = 1.54
+        self._lots = None
+        self._average = 0.0
+        self._na = 101
+        self._no = 101
+        self._ll = np.array([0.0, 0.0, 0.0])
+        self._lr = np.array([2.0, 0.0, 0.0])
+        self._ul = np.array([0.0, 2.0, 0.0])
+
+    @property
+    def radiation(self):
+        return self._radiation
+
+    @radiation.setter
+    def radiation(self, rad):
+        self._radiation = rad
+
+    @property
+    def structure(self):
+        return self._structure
+
+    @structure.setter
+    def structure(self, stru):
+        self._structure = stru
+
+    @property
+    def bins(self):
+        return self._na, self._no
+
+    @bins.setter
+    def bins(self, dims):
+        if len(dims) < 2:
+            return
+        self._na = dims[0]
+        self._no = dims[1]
+
+    @property
+    def ll(self):
+        return self._ll
+
+    @ll.setter
+    def ll(self, ll):
+        if len(ll) != 3:
+            return
+        self._ll = np.asarray(ll)
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @lr.setter
+    def lr(self, lr):
+        if len(lr) != 3:
+            return
+        self._lr = np.asarray(lr)
+
+    @property
+    def ul(self):
+        return self._ul
+
+    @ul.setter
+    def ul(self, ul):
+        if len(ul) != 3:
+            return
+        self._ul = np.asarray(ul)
 
     def calculate(self):
         """Returns a Data object"""
-        output_array = np.zeros([self.na, self.no], dtype=np.complex)
-        vector1_step = (self.lr - self.ll)/(self.na-1)
-        vector2_step = (self.ul - self.ll)/(self.no-1)
-        kx = np.zeros([self.na, self.no])
-        ky = np.zeros([self.na, self.no])
-        kz = np.zeros([self.na, self.no])
-        for x in range(self.na):
-            for y in range(self.no):
-                v = self.ll + x*vector1_step + y*vector2_step
+        output_array = np.zeros([self._na, self._no], dtype=np.complex)
+        vector1_step = (self._lr - self._ll)/(self._na-1)
+        vector2_step = (self.ul - self._ll)/(self._no-1)
+        kx = np.zeros([self._na, self._no])
+        ky = np.zeros([self._na, self._no])
+        kz = np.zeros([self._na, self._no])
+        for x in range(self._na):
+            for y in range(self._no):
+                v = self._ll + x*vector1_step + y*vector2_step
                 kx[x, y] = v[0]
                 ky[x, y] = v[1]
                 kz[x, y] = v[2]
@@ -43,7 +99,7 @@ class Fourier(object):
         # Loop of atom types
         for atomic_number in unique_atomic_numbers:
             atom_positions = positions[np.where(atomic_numbers == atomic_number)]
-            temp_array = np.zeros([self.na, self.no], dtype=np.complex)
+            temp_array = np.zeros([self._na, self._no], dtype=np.complex)
             f = periodictable.elements[atomic_number].neutron.b_c
             print("Working on atom number", atomic_number, "Total atoms:", len(atom_positions))
             # Loop over atom positions of type atomic_number
@@ -56,8 +112,8 @@ class Fourier(object):
 
     def calculate_fast(self):
         """Returns a Data object"""
-        output_array = np.zeros([self.na, self.no], dtype=np.complex)
-        kx, ky, kz = calc_k_grid(self.ll, self.lr, self.ul, self.na, self.no)
+        output_array = np.zeros([self._na, self._no], dtype=np.complex)
+        kx, ky, kz = calc_k_grid(self._ll, self._lr, self.ul, self._na, self._no)
         kx *= (2*np.pi)
         ky *= (2*np.pi)
         kz *= (2*np.pi)
@@ -69,7 +125,7 @@ class Fourier(object):
         # Loop of atom types
         for atomic_number in unique_atomic_numbers:
             atom_positions = positions[np.where(atomic_numbers == atomic_number)]
-            temp_array = np.zeros([self.na, self.no], dtype=np.complex)
+            temp_array = np.zeros([self._na, self._no], dtype=np.complex)
             f = periodictable.elements[atomic_number].neutron.b_c
             print("Working on atom number", atomic_number, "Total atoms:", len(atom_positions))
             # Loop over atom positions of type atomic_number
